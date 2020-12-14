9e076af940c87eb4e687dddb7940fa083ad73dd1
Philip Mayer <philip.mayer94@gmail.com>
2020-12-15 00:06:38 +0100

    update about page

--- frontend/src/app/pages/about/about.page.ts
+++ frontend/src/app/pages/about/about.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.page.html',
-  styleUrls: ['./about.page.scss'],
+    selector: 'app-about',
+    templateUrl: './about.page.html',
+    styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+    public data: any = [
+        { title: 'Author', desc: 'Philip Mayer'},
+        { title: 'Contact', desc: 'philip.mayer@shadowsith.de'},
+        { title: 'Year', desc: '2020-2021'},
+        { title: 'License ', desc: 'MIT', url: 'https://choosealicense.com/licenses/mit/'},
+        { title: 'Source Code', desc: 'GitHub', url: 'https://github.com/Shadowsith/FishSocializer/'}
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
