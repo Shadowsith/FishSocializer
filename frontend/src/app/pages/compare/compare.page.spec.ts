57067c78c3b291568f435c55d8a8cc868dd53d21
Philip Mayer <philip.mayer94@gmail.com>
2021-10-03 19:37:32 +0200

    update angular to v11

--- frontend/src/app/pages/compare/compare.page.spec.ts
+++ frontend/src/app/pages/compare/compare.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ComparePage } from './compare.page';
@@ -7,7 +7,7 @@ describe('ComparePage', () => {
   let component: ComparePage;
   let fixture: ComponentFixture<ComparePage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ComparePage ],
       imports: [IonicModule.forRoot()]
--- frontend/src/app/pages/fishes/fishes.page.spec.ts
+++ frontend/src/app/pages/fishes/fishes.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { FishesPage } from './fishes.page';
@@ -7,7 +7,7 @@ describe('FishesPage', () => {
   let component: FishesPage;
   let fixture: ComponentFixture<FishesPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FishesPage ],
       imports: [IonicModule.forRoot()]
